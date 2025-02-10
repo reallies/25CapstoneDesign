2e05dfcd11c0fa25e931026eae6bff726cd06187
reallies <0323eric@naver.com>
2025-02-10 17:38:49 +0900

    rd port to 8080

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // JSON 요청 파싱 미들웨어
 app.use(express.json());
