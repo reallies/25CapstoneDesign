8a0997bab09ab849705f560705d76bec72909f32
hyeonn9 <1319ono@gmail.com>
2025-03-14 22:12:29 +0900

    apply .gitinore

--- backend/src/routes/scheduleRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const tripController = require("../controller/scheduleController");
-
-const router = express.Router();
-
-//여정추가
-router.post("/",tripController.createTrip);
-
-module.exports = router;
