f24beac068fdacc3581f1fefaea96758b05a4b16
mamemomif <yskang009@gmail.com>
2025-04-08 13:15:56 +0900

    Update NavigationBar.js

--- frontend/src/scripts/components/NavigationBar.js
+++ frontend/src/scripts/components/NavigationBar.js
@@ -6,20 +6,22 @@ import logo from "../../assets/images/logo.svg";
 import user from "../../assets/images/user.svg";
 import global from "../../assets/images/global.svg";
 import search2 from "../../assets/images/search2.svg";
+import LoginModal from "./LoginModal";
 
 const NavigationBar = () => {
   const navigate = useNavigate();
   const [searchText, setSearchText] = useState("");
   const [isFocused, setIsFocused] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <div className="navbar">
-      {/* 로고 (클릭 시 홈으로 이동) */}
-      <div style={{ width: "200px", height: "auto", cursor: "pointer" }} onClick={() => navigate("/")}>
-        <img className="nav-logo" src={logo} alt="logo" style={{ width: "80%", height: "auto" }} />
+      {/* 로고 */}
+      <div style={{ width: "200px", cursor: "pointer" }} onClick={() => navigate("/")}>
+        <img className="nav-logo" src={logo} alt="logo" style={{ width: "80%" }} />
       </div>
 
-      {/* 네비게이션 메뉴 */}
+      {/* 메뉴 */}
       <div className="nav-menu">
         <span onClick={() => navigate("/")}>홈</span>
         <span onClick={() => navigate("/schedules")}>여행 일정</span>
@@ -42,11 +44,19 @@ const NavigationBar = () => {
         <img src={search2} alt="search2" />
       </div>
 
-      {/* 프로필 및 다국어 지원 */}
+      {/* 프로필 + 다국어 */}
       <div className="nav-icons">
-        <img src={user} alt="profile" onClick={() => navigate("/login")} />
+        <img
+          src={user}
+          alt="profile"
+          onClick={() => setIsModalOpen(true)} // (임시) 로그인 창 띄우지 X -> 로그인 모달만 열도록 설정(UI 확인용)
+          style={{ cursor: "pointer" }}
+        />
         <img src={global} alt="global" />
       </div>
+
+      {/* (임시) 로그인 모달 띄우기용 */}
+      <LoginModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </div>
   );
 };
