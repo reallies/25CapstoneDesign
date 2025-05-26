0e076eac87b592cfc0a867470a6630b84b61dbdf
hyeonn9 <1319ono@gmail.com>
2025-05-26 19:22:34 +0900

    fix timePickerView

--- frontend/src/scripts/pages/Schedule.js
+++ frontend/src/scripts/pages/Schedule.js
@@ -364,8 +364,8 @@ const Schedule = () => {
                                                                                         <div className="item-content">
                                                                                             <div className="item-content2">
                                                                                                 <div className="place-type">{item.placeType}</div>
-                                                                                                <div className="time" onClick={() => handleDayplaceTime(Number(day.id.replace("day-", "")), item.dayPlaceId)}>
-                                                                                                    {dayPlaceTimeMap[item.dayPlaceId] || "time"}
+                                                                                                <div className="time-label" onClick={() => handleDayplaceTime(Number(day.id.replace("day-", "")), item.dayPlaceId)}>
+                                                                                                    {dayPlaceTimeMap[item.dayPlaceId] || "시간"}
                                                                                                 </div>
                                                                                             </div>
                                                                                             <div className="place-name">{item.name}</div>
